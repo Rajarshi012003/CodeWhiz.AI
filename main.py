21ad5e95fb727f6520597ba35a3c471b7b69c356
Raj <santu1901@kgpian.iitkgp.ac.in>
2025-06-20 13:37:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import argparse
+import logging
+
+from src.cli import main as cli_main
+
+if __name__ == "__main__":
+    # Run the CLI interface
+    cli_main() 
